a4074cb227df94c94b96b004a0da886b2f199807
KushRohra <kushrohra13@gmail.com>
2021-04-24 16:59:44 +0530

    url routing in core library

---
+++ app/controllers/Pages.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Pages {
+        public function __construct() {
+            echo 'Pages loaded...';
+        }
+
+        public function index() {
+
+        }
+
+        public function about($id) {
+            echo $id;
+        }
+    }
+
+?>
