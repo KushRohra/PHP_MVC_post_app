42a2c7bfd30c64b1d7bb32297c360ca753173ca1
KushRohra <kushrohra13@gmail.com>
2021-04-24 17:26:13 +0530

    create a config file for constants and autoload all the libraries

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    // App root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL ROOT
+    define('URLROOT', 'http://localhost/mvc_kush/');
+
+    // SITE NAME
+    define('SITENAME', 'MVC_Kush');
+?>
