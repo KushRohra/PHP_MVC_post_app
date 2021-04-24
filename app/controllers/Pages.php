e895e28238e4b4df0965c38232f019e151ad20ae
KushRohra <kushrohra13@gmail.com>
2021-04-24 17:33:57 +0530

    create header and footer and include them in all views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -11,7 +11,8 @@
         }
 
         public function about() {
-            $this->view('pages/about');
+            $data = ['title' => 'About Us'];
+            $this->view('pages/about', $data);
         }
     }
 
