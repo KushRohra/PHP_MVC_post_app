46ea7aafbe86cf828273a59b4b65ca090b1d743c
KushRohra <kushrohra13@gmail.com>
2021-04-24 23:00:08 +0530

    database controller created

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,17 @@
 <?php
 
+    // DB PARAMS
+    define('DB_HOST', 'localhost');
+    define('DB_USER', '_YOUR_USER_');
+    define('DB_PASS', '_YOUR_PASS_');
+    define('DB_NAME', '_YOUR_DBNAME_');
+
     // App root
     define('APPROOT', dirname(dirname(__FILE__)));
 
     // URL ROOT
-    define('URLROOT', 'http://localhost/mvc_kush/');
+    define('URLROOT', '_YOUR_URL_');
 
     // SITE NAME
-    define('SITENAME', 'MVC_Kush');
+    define('SITENAME', '_YOUR_SITENAME_');
 ?>
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,16 +2,19 @@
 
     class Pages extends Controller {
         public function __construct() {
-
         }
 
         public function index() {
-            $data = ['title' => 'Welcome'];
+            $data = [
+                'title' => 'Welcome'
+            ];
             $this->view('pages/index', $data);
         }
 
         public function about() {
-            $data = ['title' => 'About Us'];
+            $data = [
+                'title' => 'About Us'
+            ];
             $this->view('pages/about', $data);
         }
     }
