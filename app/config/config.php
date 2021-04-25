f06bc6eb8812bafb7e16c2bc1e2218641a6d6f86
KushRohra <kushrohra13@gmail.com>
2021-04-25 15:00:47 +0530

    bootstrap and navbar added

--- app/config/config.php
+++ app/config/config.php
@@ -2,16 +2,19 @@
 
     // DB PARAMS
     define('DB_HOST', 'localhost');
-    define('DB_USER', '_YOUR_USER_');
-    define('DB_PASS', '_YOUR_PASS_');
-    define('DB_NAME', '_YOUR_DBNAME_');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'shareposts');
 
     // App root
     define('APPROOT', dirname(dirname(__FILE__)));
 
     // URL ROOT
-    define('URLROOT', '_YOUR_URL_');
+    define('URLROOT', 'http://localhost/mvc_kush');
 
     // SITE NAME
-    define('SITENAME', '_YOUR_SITENAME_');
+    define('SITENAME', 'SharePosts');
+
+    // App Version
+    define('APP_VERSION', '1.0.0')
 ?>
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -6,14 +6,16 @@
 
         public function index() {
             $data = [
-                'title' => 'Welcome'
+                'title' => 'SharePosts',
+                'description' => 'Simple social network built on a MVC framework'
             ];
             $this->view('pages/index', $data);
         }
 
         public function about() {
             $data = [
-                'title' => 'About Us'
+                'title' => 'About Us',
+                'description' => 'App to share posts with other users'
             ];
             $this->view('pages/about', $data);
         }
