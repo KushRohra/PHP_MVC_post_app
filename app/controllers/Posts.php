fb0b8b0141eeabc859ba972ae848e24c0b48a6db
KushRohra <kushrohra13@gmail.com>
2021-05-01 17:31:14 +0530

    added the posts form

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -18,6 +18,15 @@
 
             $this->view('posts/index', $data);
         }
+
+        public function add() {
+            $data = [
+                'title' => '',
+                'body' => ''
+            ];
+
+            $this->view('posts/add', $data);
+        }
     }
 
 ?>
